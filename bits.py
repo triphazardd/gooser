cbe0248681f54339a1470378287926a9125ebd89
trip <87188776+triphazardd@users.noreply.github.com>
2021-08-20 16:44:37 +0100

    ur mum

--- bits.py
+++ bits.py
@@ -1,8 +1,26 @@
+#maybe when i get a new laptop i should migrate my code here idk
+
+import discord
+import os
+import discord.ext
+from discord.ext import commands
+prefix="Gooser "
+token=token
+client = commands.Bot(command_prefix=prefix) #i'm not sure which one I actually need and so if I have both ik it works somehow, this might be that actual problem
+bot = commands.Bot(command_prefix=prefix)
+bot.remove_command('help')
+
+standardmum = 5
+ur mum = ######come up with something for this
+
 @bot.command()
-async def hug(ctx, *, user: discord.Member = None):
-    user = user 
-    embed = make_embed(title= "", desc=f"Gooser gives **{user}** a big hug on behalf of **{ctx.author}**")
-    await ctx.send(embed=embed)
-    
-#don't forget to define everything but if i'm just dumping this in gooser it should be already done, if not, back to square one lol 
-#also work out: how to remove tags (see the hi test goose command maybe), add the cute gif to it, tips in the footer of the embed?,
+async def urmum(ctx):
+    if urmum > standardmum:
+        embed = discord.Embed(title= "ur mum wide", description= "haha")
+        ctx.send(embed=embed)
+    else:
+        embed = discord.Embed(title= "", desc = "your mother is a wonderful person and I'd love to spend more time with her")
+        ctx.send(embed=embed)
+ 
+bot.run(token)
+client.run(token) #see line 9 i guess
